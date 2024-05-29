6eefcfa4f90a24a59eb6cad5937fbc2da5b3f8ac
ric-evans <emejqz@gmail.com>
2024-05-29 16:43:32 -0500

    misc

--- tests/integration/test_initial_workflows.py
+++ tests/integration/test_initial_workflows.py
@@ -103,7 +103,7 @@ async def test_100__mqgroup_activation__error_404(rc: RestClient) -> None:
     assert e.value.response.status_code == 404
 
 
-async def test_110__mqgroup_activation__error_404(rc: RestClient) -> None:
+async def test_110__mqgroup_get__error_404(rc: RestClient) -> None:
     """Test erroneous calls--logical errors (type-checking done by openapi)."""
     openapi_spec = await query_for_schema(rc)
 
