5d45550791d5f5638a55b1469b77a94bb9965278
ric-evans <emejqz@gmail.com>
2024-06-26 11:49:55 -0400

    Revert "fix handler route"
    This reverts commit a27d19fce2f57c7e581a9f615765f26d4fd8b90f.

--- mqs/rest_handlers/jwks_handlers.py
+++ mqs/rest_handlers/jwks_handlers.py
@@ -1,6 +1,7 @@
 """REST handlers for the JWKS endpoint."""
 
 import logging
+import re
 
 from rest_tools.server import validate_request
 
@@ -17,7 +18,7 @@ LOGGER = logging.getLogger(__name__)
 class WellKnownJWKSDotJSONHandler(BaseMQSHandler):
     """Handles the JWKS JSON response."""
 
-    ROUTE = r"/.well-known/jwks.json$"
+    ROUTE = re.escape("/.well-known/jwks.json") + "$"
 
     @rest_auth.service_account_auth(roles=rest_auth.ALL_AUTH_ACCOUNTS)  # type: ignore
     @validate_request(config.REST_OPENAPI_SPEC)  # type: ignore[misc]
