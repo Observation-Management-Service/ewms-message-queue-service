c767287bac1e4fa82a257721e9358ce799691daf
ric-evans <emejqz@gmail.com>
2024-06-27 13:06:31 -0400

    test with key refreshing

--- tests/integration/test_jwks.py
+++ tests/integration/test_jwks.py
@@ -11,6 +11,8 @@ import requests
 from rest_tools.client import RestClient
 from rest_tools.utils.auth import _AuthValidate
 
+from .utils import refresh_mqbroker_key_files
+
 LOGGER = logging.getLogger(__name__)
 
 ROUTE_VERSION_PREFIX = "v0"
@@ -86,42 +88,49 @@ class OpenIDAuthWithProviderInfo(_AuthValidate):
 async def test_jwks(rc: RestClient):
     """Test a normal interaction."""
 
-    # get jwks
-    auth = OpenIDAuthWithProviderInfo(
-        rc.address,
-        {
-            "jwks_uri": urljoin(rc.address, "mqbroker-issuer/.well-known/jwks.json"),
-        },
-    )
     with open(os.environ["BROKER_QUEUE_AUTH_PUBLIC_KEY_FILE"], "rb") as f:
-        assert [
+        public_key = f.read()
+    all_public_keys = [public_key]
+
+    for i in range(5):  # arbitrary no. of loops
+        if i > 0:
+            public_key, _ = refresh_mqbroker_key_files()  # change keys
+            all_public_keys.append(public_key)
+
+        # get jwks
+        auth = OpenIDAuthWithProviderInfo(
+            rc.address,
+            {
+                "jwks_uri": urljoin(
+                    rc.address, "mqbroker-issuer/.well-known/jwks.json"
+                ),
+            },
+        )
+        assert sorted(
             obj.public_bytes(
                 cryptography.hazmat.primitives.serialization.Encoding.PEM,
                 cryptography.hazmat.primitives.serialization.PublicFormat.SubjectPublicKeyInfo,
             )
             for obj in auth.public_keys.values()
-        ] == [f.read()]
+        ) == sorted(all_public_keys)
 
-    # get jwt(s) & validate
-    workflow_id = "abc123"
-    queue_aliases = ["queue1", "queue2", "queue3"]
-    public = ["queue1", "queue3"]
-    # -> reserve mq group
-    await rc.request(
-        "POST",
-        f"/{ROUTE_VERSION_PREFIX}/mqs/workflows/{workflow_id}/mq-group/reservation",
-        {"queue_aliases": queue_aliases, "public": public},
-    )
-    # -> activate mq group
-    resp = await rc.request(
-        "POST",
-        f"/{ROUTE_VERSION_PREFIX}/mqs/workflows/{workflow_id}/mq-group/activation",
-        {"criteria": {"priority": 99}},
-    )
-    # -> validate jwt(s)
-    for mqprofile in resp["mqprofiles"]:
-        print(mqprofile["auth_token"])
-        assert auth.validate(mqprofile["auth_token"])
-
-    # TODO - repeat with changed keys
-    # public_key, private_key = refresh_mqbroker_key_files()
+        # get jwt(s) & validate
+        workflow_id = "abc123"
+        queue_aliases = ["queue1", "queue2", "queue3"]
+        public = ["queue1", "queue3"]
+        # -> reserve mq group
+        await rc.request(
+            "POST",
+            f"/{ROUTE_VERSION_PREFIX}/mqs/workflows/{workflow_id}/mq-group/reservation",
+            {"queue_aliases": queue_aliases, "public": public},
+        )
+        # -> activate mq group
+        resp = await rc.request(
+            "POST",
+            f"/{ROUTE_VERSION_PREFIX}/mqs/workflows/{workflow_id}/mq-group/activation",
+            {"criteria": {"priority": 99}},
+        )
+        # -> validate jwt(s)
+        for mqprofile in resp["mqprofiles"]:
+            print(mqprofile["auth_token"])
+            assert auth.validate(mqprofile["auth_token"])
