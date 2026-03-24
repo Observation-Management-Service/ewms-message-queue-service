948f4698356f349fdf3fb33c92dd419745c608c9
ric-evans <emejqz@gmail.com>
2026-03-24 17:53:46 -0500

    ty ignores

--- mqs/rest_handlers/base_handlers.py
+++ mqs/rest_handlers/base_handlers.py
@@ -16,7 +16,7 @@ LOGGER = logging.getLogger(__name__)
 class BaseMQSHandler(RestHandler):  # pylint: disable=W0223
     """BaseMQSHandler is a RestHandler for all MQS routes."""
 
-    def initialize(  # type: ignore[override]
+    def initialize(  # type: ignore[override]  # ty: ignore[invalid-method-override]
         self,
         mongo_client: AsyncMongoClient,
         mqbroker_auth: BrokerQueueAuth,
